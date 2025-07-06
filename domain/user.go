65d406266efb3a92c607eb4db8aaa26092e54fe1
andremaesha <andremaesha@gmail.com>
2025-07-06 16:36:24 +0700

    update field naming in User struct

--- domain/user.go
+++ domain/user.go
@@ -8,15 +8,15 @@ import (
 const TableUser = "dbo.users"
 
 type User struct {
-	Id          int        `gorm:"primaryKey;autoIncrement" redis:"user_id"`
-	Verified    bool       `gorm:"default:false"`
-	FirstName   string     `gorm:"not null"`
-	LastName    string     `gorm:"not null"`
-	Email       string     `gorm:"unique;not null" redis:"email"`
-	Password    string     `gorm:"not null"`
-	AvatarId    int        `gorm:"default:NULL"`
-	Role        string     `gorm:"default:'user'" redis:"role"`
-	CreatedDate *time.Time `gorm:"default:NULL"`
+	Id        int        `gorm:"primaryKey;autoIncrement" redis:"user_id"`
+	Verified  bool       `gorm:"default:false"`
+	FirstName string     `gorm:"not null"`
+	LastName  string     `gorm:"not null"`
+	Email     string     `gorm:"unique;not null" redis:"email"`
+	Password  string     `gorm:"not null"`
+	AvatarId  int        `gorm:"default:NULL"`
+	Role      string     `gorm:"default:'user'" redis:"role"`
+	CreatedAt *time.Time `gorm:"default:NULL"`
 }
 
 type UserRepository interface {
