a981192e0dead292b7ba557cd9a4f0ff08d31061
andremaesha <andremaesha@gmail.com>
2025-07-06 16:41:06 +0700

    add ValidateUserVerified method to LoginUsecase interface

--- domain/login.go
+++ domain/login.go
@@ -15,6 +15,7 @@ type LoginResponse struct {
 
 type LoginUsecase interface {
 	GetUserByEmail(c context.Context, email string) (*User, error)
+	ValidateUserVerified(verified bool) error
 	SetSession(c context.Context, expire int, idSession string, data *User) error
 	EncryptSession(key, data string) string
 }
